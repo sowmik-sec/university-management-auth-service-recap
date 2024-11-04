3d05b6fe502217f8b9ad28abaee4b36f23d053a8
sowmik-sec <sowmik.sec@gmail.com>
2024-11-04 22:17:28 +0600

    create user route, controller test utility function

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express, { Application, Request, Response } from 'express';
 const app: Application = express();
 import cors from 'cors';
+import userRouter from './app/modules/user/user.route';
 app.use(cors());
 
 //parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// application routes
+app.use('/api/v1/users', userRouter);
 
 // Testing
 app.get('/', (req: Request, res: Response) => {
