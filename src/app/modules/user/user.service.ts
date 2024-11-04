9d412580fd46f7f3380ea6c7c2dbb3c8b1023f03
sowmik-sec <sowmik.sec@gmail.com>
2024-11-04 20:13:03 +0600

    Create generateUser utility function

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,8 +1,18 @@
 import { User } from './user.model';
 import { IUser } from './user.interface';
+import config from '../../../config';
+import { generateUserId } from './user.utils';
 
 const createUser = async (user: IUser): Promise<IUser | null> => {
   // auto generated incremental id
+  const id = await generateUserId();
+  user.id = id;
+  // default password
+
+  if (!user.password) {
+    user.password = config.default_user_pass as string;
+  }
+
   const createdUser = await User.create(user);
   if (!createdUser) {
     throw new Error('Failed to create user');
