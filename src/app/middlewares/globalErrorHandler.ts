780aac3f2b967c91b79b4853d7a18042207f8641
sowmik-sec <sowmik.sec@gmail.com>
2024-11-06 19:43:22 +0600

    Refactor global error handler

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,6 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { NextFunction, Request, Response } from 'express';
 import { IGenericErrorMessage } from '../../interfaces/error';
 import config from '../../config';
+import handleValidationError from '../../errors/handleValidationError';
+import ApiError from '../../errors/ApiError';
+import { Error } from 'mongoose';
 
 const globalErrorHandler = (
   err,
@@ -11,6 +16,34 @@ const globalErrorHandler = (
   let statusCode = 500;
   let message = 'Something went wrong';
   let errorMessages: IGenericErrorMessage[] = [];
+
+  if (err?.name === 'ValidationError') {
+    const simplifiedError = handleValidationError(err);
+    statusCode = simplifiedError.statusCode;
+    message = simplifiedError.message;
+    errorMessages = simplifiedError.errorMessages;
+  } else if (err instanceof ApiError) {
+    statusCode = err?.statusCode;
+    message = err?.message;
+    errorMessages = err?.message
+      ? [
+          {
+            path: '',
+            message: err?.message,
+          },
+        ]
+      : [];
+  } else if (err instanceof Error) {
+    message = err?.message;
+    errorMessages = err?.message
+      ? [
+          {
+            path: '',
+            message: err?.message,
+          },
+        ]
+      : [];
+  }
 
   res.status(statusCode).json({
     success: false,
