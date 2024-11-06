9a22f1c48611fe64e6a8d144412c358693cd3932
sowmik-sec <sowmik.sec@gmail.com>
2024-11-06 10:41:57 +0600

    different ways to handle error

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, NextFunction, Request, Response } from 'express';
 const app: Application = express();
 import cors from 'cors';
 import userRouter from './app/modules/user/user.route';
@@ -11,9 +11,34 @@ app.use(express.urlencoded({ extended: true }));
 // application routes
 app.use('/api/v1/users', userRouter);
 
+class ApiError extends Error {
+  statusCode: number;
+  constructor(statusCode: number, message: string | undefined, stack = '') {
+    super(message);
+    this.statusCode = statusCode;
+    if (stack) {
+      this.stack = stack;
+    } else {
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+}
+
 // Testing
-app.get('/', (req: Request, res: Response) => {
-  res.send('Working Successfully!');
+app.get('/', (req: Request, res: Response, next: NextFunction) => {
+  // res.send('Working Successfully!');
+  throw new Error('Ore baba error hoiche');
+  next('Ore baba error');
+});
+
+// global error handler
+app.use((err, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    res.status(400).json({ error: err });
+  } else {
+    res.status(500).json({ err: 'Something went wrong' });
+  }
+  console.log(err);
 });
 
 export default app;
