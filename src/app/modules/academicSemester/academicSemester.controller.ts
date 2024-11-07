1d71c33bbf8f132f793a62a1520e1a94b393f877
sowmik-sec <sowmik.sec@gmail.com>
2024-11-07 20:27:14 +0600

    Optimize Try-Catch Block and Api Response

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -1,20 +1,25 @@
-/* eslint-disable @typescript-eslint/restrict-template-expressions */
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { AcademicSemesterService } from './academicSemester.service';
 import catchAsync from '../../../shared/catchAsync';
+import sendResponse from '../../../shared/sendResponse';
+import { StatusCodes } from 'http-status-codes';
 
-const createSemester = catchAsync(async (req: Request, res: Response) => {
-  const { ...academicSemesterData } = req.body;
-  const result =
-    await AcademicSemesterService.createSemester(academicSemesterData);
-  res.status(200).json({
-    success: true,
-    message: 'Academic Semester created successfully',
-    data: result,
-  });
-});
+const createSemester = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { ...academicSemesterData } = req.body;
+    const result =
+      await AcademicSemesterService.createSemester(academicSemesterData);
+    next();
+    sendResponse(res, {
+      success: true,
+      statusCode: StatusCodes.OK,
+      message: 'Academic Semester created successfully',
+      data: result,
+    });
+  },
+);
 
 export const AcademicSemesterController = {
   createSemester,
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,19 +1,25 @@
-/* eslint-disable @typescript-eslint/restrict-template-expressions */
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { UserService } from './user.service';
 import catchAsync from '../../../shared/catchAsync';
+import sendResponse from '../../../shared/sendResponse';
+import { StatusCodes } from 'http-status-codes';
 
-const createUser = catchAsync(async (req: Request, res: Response) => {
-  const { user } = req.body;
-  const result = await UserService.createUser(user);
-  res.status(200).json({
-    success: true,
-    message: 'User created successfully',
-    data: result,
-  });
-});
+const createUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const { user } = req.body;
+    const result = await UserService.createUser(user);
+    next();
+
+    sendResponse(res, {
+      success: true,
+      statusCode: StatusCodes.OK,
+      message: 'User created successfully',
+      data: result,
+    });
+  },
+);
 
 export const UserController = {
   createUser,
