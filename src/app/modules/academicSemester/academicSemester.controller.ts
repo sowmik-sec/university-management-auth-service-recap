7ee713d90ac34a636a6ef09f6ff4d4c6f1b08ad5
sowmik-sec <sowmik.sec@gmail.com>
2024-11-10 20:43:17 +0600

    get single semester from db

--- src/app/modules/academicSemester/academicSemester.controller.ts
+++ src/app/modules/academicSemester/academicSemester.controller.ts
@@ -44,7 +44,22 @@ const getAllSemesters = catchAsync(
   },
 );
 
+const getSingleSemester = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const id = req.params.id;
+    const result = await AcademicSemesterService.getSingleSemester(id);
+    sendResponse<IAcademicSemester>(res, {
+      success: true,
+      statusCode: StatusCodes.OK,
+      message: 'Academic Semesters retrieved successfully',
+      data: result,
+    });
+    next();
+  },
+);
+
 export const AcademicSemesterController = {
   createSemester,
   getAllSemesters,
+  getSingleSemester,
 };
