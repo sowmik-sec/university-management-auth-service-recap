97e1a3a0d37d1cc2e1de69e8063a7313f18af112
sowmik-sec <sowmik.sec@gmail.com>
2024-11-12 21:51:07 +0600

    create academic department interface, model and zod validation

---
+++ src/app/modules/academicDepartment/academicDepartment.interface.ts
@@ -0,0 +1,13 @@
+import { Model, Types } from 'mongoose';
+import { IAcademicFaculty } from '../academicFaculty/academicFaculty.interface';
+
+export type IAcademicDepartment = {
+  title: string;
+  academicFaculty: Types.ObjectId | IAcademicFaculty;
+  syncId: string;
+};
+
+export type AcademicDepartmentModel = Model<
+  IAcademicDepartment,
+  Record<string, unknown>
+>;
---
+++ src/app/modules/academicDepartment/academicDepartment.model.ts
@@ -0,0 +1,39 @@
+import { model, Schema } from 'mongoose';
+import {
+  AcademicDepartmentModel,
+  IAcademicDepartment,
+} from './academicDepartment.interface';
+
+const AcademicDepartmentSchema = new Schema<
+  IAcademicDepartment,
+  AcademicDepartmentModel
+>(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    academicFaculty: {
+      type: Schema.Types.ObjectId,
+      ref: 'AcademicFaculty',
+      required: true,
+    },
+    syncId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+    },
+  },
+);
+
+export const AcademicDepartment = model<
+  IAcademicDepartment,
+  AcademicDepartmentModel
+>('AcademicDepartment', AcademicDepartmentSchema);
