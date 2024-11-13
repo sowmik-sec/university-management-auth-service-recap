be6e1496d40afc0844499f40b625909fe1a12dcf
sowmik-sec <sowmik.sec@gmail.com>
2024-11-13 20:56:20 +0600

    Dynamically update guardian , local guardian , test API_s , Fix Bugs

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -81,7 +81,7 @@ const updateStudent = async (
   if (!isExist) {
     throw new ApiError(StatusCodes.NOT_FOUND, 'Student not found');
   }
-  const { name, gurdian, localGuardian, ...studentData } = payload;
+  const { name, guardian, localGuardian, ...studentData } = payload;
   const updatedStudentData: Partial<IStudent> = { ...studentData };
 
   /* const name = {
@@ -97,8 +97,22 @@ const updateStudent = async (
       (updatedStudentData as any)[nameKey] = name[key as keyof typeof name];
     });
   }
+  if (guardian && Object.keys(guardian).length > 0) {
+    Object.keys(guardian).forEach(key => {
+      const guardianKey = `guardian.${key}`;
+      (updatedStudentData as any)[guardianKey] =
+        guardian[key as keyof typeof guardian];
+    });
+  }
+  if (localGuardian && Object.keys(localGuardian).length > 0) {
+    Object.keys(localGuardian).forEach(key => {
+      const localGuardianKey = `localGuardian.${key}`;
+      (updatedStudentData as any)[localGuardianKey] =
+        localGuardian[key as keyof typeof localGuardian];
+    });
+  }
 
-  const result = await Student.findOneAndUpdate({ _id: id }, payload, {
+  const result = await Student.findOneAndUpdate({ id }, updatedStudentData, {
     new: true,
   });
   return result;
