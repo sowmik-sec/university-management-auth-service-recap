dc371e9f4b02a4625a2d265d7e4e68ac0639bed9
sowmik-sec <sowmik.sec@gmail.com>
2024-11-15 11:48:20 +0600

    update constants for admin

--- src/app/modules/admin/admin.constant.ts
+++ src/app/modules/admin/admin.constant.ts
@@ -2,20 +2,23 @@ export const gender = ['male', 'female'];
 
 export const bloodGroup = ['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-'];
 
-export const adminSearchableFields = [
-  'id',
-  'email',
-  'name.firstName',
-  'name.middleName',
-  'name.lastName',
-  'contactNo',
-];
-
 export const adminFilterableFields = [
   'searchTerm',
   'id',
+  'gender',
   'bloodGroup',
   'email',
   'contactNo',
   'emergencyContactNo',
+  'managementDepartment',
+  'designation',
+];
+
+export const adminSearchableFields = [
+  'email',
+  'contactNo',
+  'emergencyContactNo',
+  'name.firstName',
+  'name.lastName',
+  'name.middleName',
 ];
