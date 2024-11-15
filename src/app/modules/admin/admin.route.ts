51f57d1247c3fdbcb3ff4e6351dbc5c1d35ab5d1
sowmik-sec <sowmik.sec@gmail.com>
2024-11-15 11:51:56 +0600

    create routes for admin

---
+++ src/app/modules/admin/admin.route.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import { AdminValidation } from './admin.validation';
+import validateRequest from '../../middlewares/validateRequest';
+const router = express.Router();
+
+router.get('/', AdminController.getAllAdmins);
+
+router.get('/:id', AdminController.getSingleAdmin);
+
+router.patch(
+  '/:id',
+  validateRequest(AdminValidation.updateAdminZodSchema),
+  AdminController.updateAdmin,
+);
+
+export const AdminRoutes = router;
