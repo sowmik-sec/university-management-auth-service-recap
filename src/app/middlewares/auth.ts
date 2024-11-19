86f58c5fd4762baf0ce84bdb83854b744ccb4a47
sowmik-sec <sowmik.sec@gmail.com>
2024-11-19 22:07:56 +0600

    Authentication , authorization & auth middleware

---
+++ src/app/middlewares/auth.ts
@@ -0,0 +1,8 @@
+import { NextFunction, Request, Response } from 'express';
+
+const auth = () => async (req: Request, res: Response, next: NextFunction) => {
+  try {
+  } catch (error) {
+    next(error);
+  }
+};
--- src/app/modules/academicFaculty/academicFaculty.controller.ts
+++ src/app/modules/academicFaculty/academicFaculty.controller.ts
@@ -24,6 +24,7 @@ const createFaculty = catchAsync(async (req: Request, res: Response) => {
 });
 
 const getAllFaculties = catchAsync(async (req: Request, res: Response) => {
+  console.log(req.headers.authorization);
   const filters = pick(req.query, academicFacultyFilterableFields);
   const paginationOptions = pick(req.query, paginationFields);
   const result = await FacultyService.getAllFaculties(
