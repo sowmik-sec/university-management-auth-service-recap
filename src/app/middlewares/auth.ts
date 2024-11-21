94d245c81de455d6c03183d24b9e3b5c15e95b79
sowmik-sec <sowmik.sec@gmail.com>
2024-11-21 19:30:10 +0600

    Create auth middleware

--- src/app/middlewares/auth.ts
+++ src/app/middlewares/auth.ts
@@ -1,10 +1,36 @@
+/* eslint-disable @typescript-eslint/restrict-template-expressions */
 import { NextFunction, Request, Response } from 'express';
+import ApiError from '../../errors/ApiError';
+import { StatusCodes } from 'http-status-codes';
+import { jwtHelpers } from '../../helpers/jwtHelpers';
+import config from '../../config';
+import { Secret } from 'jsonwebtoken';
 
-const auth = () => async (req: Request, res: Response, next: NextFunction) => {
-  try {
-  } catch (error) {
-    next(error);
-  }
-};
+const auth =
+  (...requiredRoles: string[]) =>
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      // get authorization token
+      const token = req.headers.authorization;
+      if (!token) {
+        throw new ApiError(StatusCodes.UNAUTHORIZED, 'You are not authorized');
+      }
+      // verify token
+      let verifiedUser = null;
+      try {
+        verifiedUser = jwtHelpers.verifyToken(
+          token,
+          config.jwt.secret as Secret,
+        );
+        console.log(verifiedUser);
+      } catch (err) {
+        throw new ApiError(StatusCodes.FORBIDDEN, `Invalid token ${err}`);
+      }
+
+      req.user = verifiedUser;
+    } catch (error) {
+      next(error);
+    }
+  };
 
 export default auth;
