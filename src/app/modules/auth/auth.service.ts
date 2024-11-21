b282bfd5474c303af054a9a12407826959ae9451
sowmik-sec <sowmik.sec@gmail.com>
2024-11-21 11:11:51 +0600

    Check User Existence and Validate Password

--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,6 +1,8 @@
-import { IAdmin } from '../admin/admin.interface';
+import { StatusCodes } from 'http-status-codes';
+import ApiError from '../../../errors/ApiError';
 import { User } from '../user/user.model';
 import { ILoginUser } from './auth.interface';
+import bcrypt from 'bcrypt';
 
 const loginUser = async (payload: ILoginUser) => {
   const { id, password } = payload;
@@ -8,7 +10,19 @@ const loginUser = async (payload: ILoginUser) => {
   const isUserExist = await User.findOne(
     { id },
     { id: 1, password: 1, needsPasswordChange: 1 },
+  ).lean();
+  if (!isUserExist) {
+    throw new ApiError(StatusCodes.NOT_FOUND, 'User does not exist');
+  }
+  // match password
+  const isPasswordMatched = await bcrypt.compare(
+    password,
+    isUserExist.password,
   );
+  if (!isPasswordMatched) {
+    throw new ApiError(StatusCodes.UNAUTHORIZED, 'Password is incorrect');
+  }
+  // create access token
 
   return {};
 };
