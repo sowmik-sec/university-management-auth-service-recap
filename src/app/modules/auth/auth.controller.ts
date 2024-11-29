c1be15b9a6733a32602142f0d2b744198afb0106
sowmik-sec <sowmik.sec@gmail.com>
2024-11-29 08:57:41 +0600

    create password change route and controller

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -47,13 +47,12 @@ const refreshToken = catchAsync(async (req: Request, res: Response) => {
 const changePassword = catchAsync(async (req: Request, res: Response) => {
   const user = req.user;
   const { ...passwordData } = req.body;
-  const result = await AuthService.changePassword(passwordData, user);
+  await AuthService.changePassword(user, passwordData);
 
   sendResponse<ILoginUserResponse>(res, {
     statusCode: StatusCodes.OK,
     success: true,
     message: 'Password changed successfully',
-    data: result,
   });
 });
 
--- src/app/modules/auth/auth.route.ts
+++ src/app/modules/auth/auth.route.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import validateRequest from '../../middlewares/validateRequest';
 import { AuthValidation } from './auth.validation';
 import { AuthController } from './auth.controller';
+import auth from '../../middlewares/auth';
+import { Enum_USER_ROLE } from '../../../enums/user';
 const router = express.Router();
 
 router.post(
@@ -17,6 +19,12 @@ router.post(
 router.post(
   '/change-password',
   validateRequest(AuthValidation.changePasswordZodSchema),
+  auth(
+    Enum_USER_ROLE.ADMIN,
+    Enum_USER_ROLE.SUPER_ADMIN,
+    Enum_USER_ROLE.STUDENT,
+    Enum_USER_ROLE.FACULTY,
+  ),
   AuthController.changePassword,
 );
 
